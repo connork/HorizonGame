cb17da80bafad08f5ca48199d2622bfbd51f3e93
RoyAwesome <madKylegar@gmail.com>
2012-05-13 14:30:00 -0700

    Work on movement mode

--- Assets/HorizonGame/scripts/Movement/WalkingCharacterController.cs
+++ Assets/HorizonGame/scripts/Movement/WalkingCharacterController.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 
-public class WalkingCharacterController : MonoBehaviour {
+public class WalkingCharacterController : BaseCharacterController {
     public Vector3 walkingSpeed;
 
     AnimationCurve test;
@@ -10,12 +10,21 @@ public class WalkingCharacterController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Quaternion rot = Quaternion.AngleAxis(40, new Vector3(0, 1, 0));
+      
        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        this.transform.eulerAngles = new Vector3(this.transform.eulerAngles.x, inputController.transform.eulerAngles.y, this.transform.eulerAngles.z);
+        Vector3 movement = new Vector3(-inputController.getMovementVector().z, 0, inputController.getMovementVector().x);
+        movement.Scale(walkingSpeed);
+        Debug.Log(movement);
+        this.transform.Translate(movement);
 	}
+
+    public override bool CanChangeMode()
+    {
+        return true;
+    }
 }
