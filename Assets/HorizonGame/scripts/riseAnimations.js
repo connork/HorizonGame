78f7a99044fdf962e042a343bf78325cad01e202
dastyckc <dastyckc@scf462.scf.oregonstate.edu>
2012-05-23 15:56:20 -0700

    added stuff

---
+++ Assets/HorizonGame/scripts/riseAnimations.js
@@ -0,0 +1,12 @@
+private var done = false;
+
+function Update () {
+    if(!animation.isPlaying && !done){
+        playNextAnimation();
+        done = true;
+    }
+}
+
+function playNextAnimation(){
+    animation.Play("FloatingPlatform3");
+}
