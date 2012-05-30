507471d7a01ff763f19a1d5a1e26a18a3e4117cd
fieldju <fieldju@scf461.scf.oregonstate.edu>
2012-05-30 15:24:26 -0700

    added audio assets

--- updateAssets.sh
+++ updateAssets.sh
@@ -5,3 +5,4 @@
 
 cp -r /Volumes/ClassRoomSAN-1/NMC499/ClassProject/Assets/Models/ /Users/fieldju/repo/Assets/HorizonGame/Models/ 
 
+cp -r /Volumes/ClassRoomSAN-1/NMC499/ClassProject/Assets/Audio/ /Users/fieldju/repo/Assets/HorizonGame/Audio/ 
