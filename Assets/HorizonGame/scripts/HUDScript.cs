0bbfd4bf27f423644ccf297f23a8dd6f51840880
Kyle Connor <cuda.connork@gmail.com>
2012-05-30 19:11:34 -0700

    updated HUD and menus

--- Assets/HorizonGame/scripts/HUDScript.cs
+++ Assets/HorizonGame/scripts/HUDScript.cs
@@ -1,9 +1,15 @@
+/*
+ * HUD Script
+ */
 using UnityEngine;
 using System.Collections;
 
 public class HUDScript : MonoBehaviour {
 
-    public Texture2D map_image;
+    private static float NATIVE_WIDTH = 1026;
+    private static float NATIVE_HEIGHT = 768;
+
+    public GUISkin menu_skin;
 
     public GUISkin roll_skin;
     public GUISkin walk_skin;
@@ -17,15 +23,7 @@ public class HUDScript : MonoBehaviour {
     public static Texture2D aim_mode_image;
     public static Texture2D grasp_mode_image;
 
-    /* Upgrades (Currently Not implemented)
-    public Texture2D speed_increase_image;
-    public Texture2D jump_boost_image;
-    public Texture2D extended_grapple_image;
-    public Texture2D better_grapple_image;
-    public Texture2D grounded_circuit_image;
-    */
-
-    private enum enMenuLevel {pause_menu, save_menu, load_menu, options_menu, controls_menu, no_menu};
+    private enum enMenuLevel { pause_menu, controls_menu, no_menu };
 
     private enMenuLevel menu_level = enMenuLevel.no_menu;
 
@@ -34,20 +32,19 @@ public class HUDScript : MonoBehaviour {
     private bool aim_toggle = false;
     private bool grasp_toggle = false;
 
-    /*Removed toolbar*/
-    //private int toolbar_int = 0;
-    //public Texture2D[] toolbar_strings = { roll_mode_image, walk_mode_image, aim_mode_image, grasp_mode_image };
-
-    private int selectgrid_int = 0;
-    private string[] selectgrid_strings = { "Map", "Menu" };
-
     /*Compass Stuff*/
     //Compass Textures
     public Texture2D compass_bg;
-    public Texture2D compass_bubble;
+    public Texture2D compass_north_bubble;
+    public Texture2D compass_south_bubble;
+    public Texture2D compass_east_bubble;
+    public Texture2D compass_west_bubble;
 
     //0 for + Z axis, 90 for + X axis
     public float north;
+    public float south;
+    public float east;
+    public float west;
 
     //where compass is placed
     public Vector2 center;
@@ -60,7 +57,13 @@ public class HUDScript : MonoBehaviour {
     public float radius;
 
     private Rect compass_rect;
-    private float rot, x, y;
+    private float rot_n, rot_e, rot_s, rot_w, xn, yn, xe, ye, xs, ys, xw, yw;
+
+    //Camera Storage Variables
+    private int culling_storage;
+    private CameraClearFlags flags_storage;
+
+    private bool is_game_paused;
 
     void Update() {
 
@@ -68,10 +71,16 @@ public class HUDScript : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.Escape) && (menu_level == enMenuLevel.pause_menu)) {
             menu_level = enMenuLevel.no_menu;
             Time.timeScale = 1.0f;
+            //ResetCameraValues();
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && (menu_level == enMenuLevel.controls_menu)) {
+            menu_level = enMenuLevel.pause_menu;
         }
         else if (Input.GetKeyDown(KeyCode.Escape)) {
-            menu_level = enMenuLevel.pause_menu;
             Time.timeScale = 0.0f;
+            SaveCameraValues();
+            menu_level = enMenuLevel.pause_menu;
+
         }
 
         /*track 1-4 button press*/
@@ -103,165 +112,167 @@ public class HUDScript : MonoBehaviour {
         }
 
         /*Compass Stuff*/
-        rot = (-90 + this.transform.eulerAngles.y - north) * Mathf.Deg2Rad;
+        //North Bubble Position
+        rot_n = (-90 + this.transform.eulerAngles.y - north) * Mathf.Deg2Rad;
 
-        //Bubble position
-        x = radius * Mathf.Cos(rot);
-        y = radius * Mathf.Sin(rot);
+        xn = radius * Mathf.Sin(rot_n);
+        yn = radius * Mathf.Cos(rot_n);
+        //South Bubble Position
+        rot_s = (-90 + this.transform.eulerAngles.y - south) * Mathf.Deg2Rad;
+
+        xs = radius * Mathf.Sin(rot_s);
+        ys = radius * Mathf.Cos(rot_s);
+        //East Bubble Position
+        rot_e = (-90 + this.transform.eulerAngles.y - east) * Mathf.Deg2Rad;
+
+        xe = radius * Mathf.Sin(rot_e);
+        ye = radius * Mathf.Cos(rot_e);
+        //West Bubble Position
+        rot_w = (-90 + this.transform.eulerAngles.y - west) * Mathf.Deg2Rad;
+
+        xw = radius * Mathf.Sin(rot_w);
+        yw = radius * Mathf.Cos(rot_w);
 
     }//end Update()
 
     void OnGUI() {
 
+        HUDScaleScript.BeginGUI();
+
         GUI.skin.font = game_font;
 
-        /*Begin HUD Code*/
-        GUI.skin = roll_skin;
-        GUI.Toggle(new Rect(0, (Screen.height - 75), 75, 75), roll_toggle, roll_mode_image);
-
-        GUI.skin = walk_skin;
-        GUI.Toggle(new Rect(80, (Screen.height - 75), 75, 75), walk_toggle, walk_mode_image);
-
-        GUI.skin = aim_skin;
-        GUI.Toggle(new Rect(160, (Screen.height - 75), 75, 75), aim_toggle, aim_mode_image);
-
-        GUI.skin = grasp_skin;
-        GUI.Toggle(new Rect(240, (Screen.height - 75), 75, 75), grasp_toggle, grasp_mode_image);
-
-        GUI.skin = null; //reset skin for other stuff
-        /*End HUD Code*/
-
         /*Begin Pause Menu Switch*/
-        switch(menu_level) {
+        switch (menu_level) {
 
             case enMenuLevel.pause_menu:
+                is_game_paused = true;
                 ShowPauseMenu();
-                break;
 
-            case enMenuLevel.save_menu:
-                ShowSaveMenu();
-                break;
-
-            case enMenuLevel.load_menu:
-                ShowLoadMenu();
-                break;
-
-            case enMenuLevel.options_menu:
-                ShowOptionsMenu();
                 break;
 
             case enMenuLevel.controls_menu:
                 ShowControlsMenu();
+
                 break;
 
             case enMenuLevel.no_menu:
+
+                if (is_game_paused == true) {
+                    is_game_paused = false;
+                    ResetCameraValues();
+                }
+                /*Begin HUD Code*/
+                GUI.skin = roll_skin;
+                GUI.Toggle(new Rect(0, (NATIVE_HEIGHT - 75), 75, 75), roll_toggle, roll_mode_image);
+
+                GUI.skin = walk_skin;
+                GUI.Toggle(new Rect(80, (NATIVE_HEIGHT - 75), 75, 75), walk_toggle, walk_mode_image);
+
+                GUI.skin = aim_skin;
+                GUI.Toggle(new Rect(160, (NATIVE_HEIGHT - 75), 75, 75), aim_toggle, aim_mode_image);
+
+                GUI.skin = grasp_skin;
+                GUI.Toggle(new Rect(240, (NATIVE_HEIGHT - 75), 75, 75), grasp_toggle, grasp_mode_image);
+
+                GUI.skin = null; //reset skin for other stuff
+
+                //Draw Compass Background
+                GUI.DrawTexture(new Rect(center.x - compass_size.x / 2, center.y - compass_size.y / 2, compass_size.x, compass_size.y), compass_bg);
+
+                //Draw North Bubble
+                GUI.DrawTexture(new Rect(center.x + xn - bubble_size.x / 2, center.y + yn - bubble_size.y / 2, bubble_size.x, bubble_size.y), compass_north_bubble);
+                //Draw South Bubble
+                GUI.DrawTexture(new Rect(center.x + xs - bubble_size.x / 2, center.y + ys - bubble_size.y / 2, bubble_size.x, bubble_size.y), compass_south_bubble);
+                //Draw East Bubble
+                GUI.DrawTexture(new Rect(center.x + xe - bubble_size.x / 2, center.y + ye - bubble_size.y / 2, bubble_size.x, bubble_size.y), compass_east_bubble);
+                //Draw West Bubble
+                GUI.DrawTexture(new Rect(center.x + xw - bubble_size.x / 2, center.y + yw - bubble_size.y / 2, bubble_size.x, bubble_size.y), compass_west_bubble);
+                /*End HUD Code*/
                 break;
 
         }
         /*End Pause Menu Switch*/
 
-        //Draw Compass Background
-        GUI.DrawTexture(new Rect(center.x - compass_size.x / 2, center.y - compass_size.y / 2, compass_size.x, compass_size.y), compass_bg);
-        //Draw Bubble
-        GUI.DrawTexture(new Rect(center.x + x - bubble_size.x / 2, center.y + y - bubble_size.y / 2, bubble_size.x, bubble_size.y), compass_bubble);
+        HUDScaleScript.EndGUI();
 
     }//end OnGUI()
 
+    /*
+     * ShowPauseMenu()
+     * This Function handles the display of the pause menu
+     * Includes: Resume, Controls, Exit to Main Menu
+     * Removed: Save, Load, Options
+     */
     void ShowPauseMenu() {
 
-        GUI.Box(new Rect((Screen.width / 6), 100, (Screen.width / 1.5f), 350), "Pause Menu");
+        GUI.skin = menu_skin;
+        GUI.Box(new Rect(0, 0, NATIVE_WIDTH, NATIVE_HEIGHT), "Pause Menu");
 
-        selectgrid_int = GUI.SelectionGrid(new Rect(((Screen.width / 6) + 10), 120, 100, 200), 
-                                                                        selectgrid_int, selectgrid_strings, 1);
-
-        //Show map
-        if (selectgrid_int == 0) {
-
-            GUI.DrawTexture(new Rect(((Screen.width / 3) + 10), 140, ((Screen.width / 2.2f)), 300), map_image, ScaleMode.StretchToFill);
-
+        //make "Resume" button
+        if (GUI.Button(new Rect((NATIVE_WIDTH / 2) - 250, (NATIVE_HEIGHT / 2) - 150, 480, 40), "Resume")) {
+            menu_level = enMenuLevel.no_menu;
+            Time.timeScale = 1.0f;
+            //ResetCameraValues();
         }
-        //Show Inventory
-        /*if (selectgrid_int == 1) {
 
-            GUI.Label(new Rect(((Screen.width / 3) + 10), 140, ((Screen.width / 3) - 20), 20), "Inventory");
-            GUI.DrawTexture(new Rect(((Screen.width / 3) + 10), 160, 60, 60), speed_increase_image, ScaleMode.StretchToFill);
-            GUI.DrawTexture(new Rect(((Screen.width / 3) + 10) + 65, 160, 60, 60), jump_boost_image, ScaleMode.StretchToFill);
-            GUI.DrawTexture(new Rect(((Screen.width / 3) + 10) + 130, 160, 60, 60), extended_grapple_image, ScaleMode.StretchToFill);
-            GUI.DrawTexture(new Rect(((Screen.width / 3) + 10) + 195, 160, 60, 60), better_grapple_image, ScaleMode.StretchToFill);
-            GUI.DrawTexture(new Rect(((Screen.width / 3) + 10) + 260, 160, 60, 60), grounded_circuit_image, ScaleMode.StretchToFill);
-
-        }*/
-        //Show options
-        if (selectgrid_int == 1) {
-
-            //make "Resume" button
-            if (GUI.Button(new Rect(((Screen.width / 3) + 10), 140, ((Screen.width / 3) - 20), 20), "Resume")) {
-                menu_level = enMenuLevel.no_menu;
-            }
-
-            //make "Save" button
-            if (GUI.Button(new Rect(((Screen.width / 3) + 10), 180, ((Screen.width / 3) - 20), 20), "Save")) {
-                menu_level = enMenuLevel.save_menu;
-            }
-
-            //make "Load" button
-            if (GUI.Button(new Rect(((Screen.width / 3) + 10), 220, ((Screen.width / 3) - 20), 20), "Load")) {
-                menu_level = enMenuLevel.load_menu;
-            }
-
-            //make "Options" button
-            if (GUI.Button(new Rect(((Screen.width / 3) + 10), 260, ((Screen.width / 3) - 20), 20), "Options")) {
-                menu_level = enMenuLevel.options_menu;
-            }
-
-            //make "Controls" button
-            if (GUI.Button(new Rect(((Screen.width / 3) + 10), 300, ((Screen.width / 3) - 20), 20), "Controls")) {
-                menu_level = enMenuLevel.controls_menu;
-            }
-
-            //make "Exit To Main Menu" button
-            if (GUI.Button(new Rect(((Screen.width / 3) + 10), 340, ((Screen.width / 3) - 20), 20), "Exit To Main Menu")) {
-                Application.LoadLevel(0);
-            }
-
+        //make "Controls" button
+        if (GUI.Button(new Rect((NATIVE_WIDTH / 2) - 250, (NATIVE_HEIGHT / 2) - 100, 480, 40), "Controls")) {
+            menu_level = enMenuLevel.controls_menu;
         }
+
+        //make "Exit To Main Menu" button
+        if (GUI.Button(new Rect((NATIVE_WIDTH / 2) - 250, (NATIVE_HEIGHT / 2) - 50, 480, 40), "Exit To Main Menu")) {
+            Application.LoadLevel(0);
+        }
+        GUI.skin = null;
 
     }//end GetPauseMenu()
 
-
-    void ShowSaveMenu() {
-
-        //make background box
-        GUI.Box(new Rect((Screen.width / 3), 100, (Screen.width / 3), 100), "Save Menu");
-
-        GUI.Label(new Rect(((Screen.width / 3) + 10), 140, ((Screen.width / 3) - 20), 20), "<Options Go Here>");
-
-    }//end GetOptionsMenu()
-
-    void ShowLoadMenu() {
-
-        //make background box
-        GUI.Box(new Rect((Screen.width / 3), 100, (Screen.width / 3), 100), "Load Menu");
-
-        GUI.Label(new Rect(((Screen.width / 3) + 10), 140, ((Screen.width / 3) - 20), 20), "<Options Go Here>");
-
-    }//end GetOptionsMenu()
-
-    void ShowOptionsMenu() {
-
-        //make background box
-        GUI.Box(new Rect((Screen.width / 3), 100, (Screen.width / 3), 100), "Options Menu");
-
-        GUI.Label(new Rect(((Screen.width / 3) + 10), 140, ((Screen.width / 3) - 20), 20), "<Options Go Here>");
-
-    }//end GetOptionsMenu()
-
+    /*
+     * ShowControlsMenu()
+     * This function handles the display of the controls
+     */
     void ShowControlsMenu() {
 
-        //make background box
-        GUI.Box(new Rect((Screen.width / 3), 100, (Screen.width / 3), 100), "Controls Menu");
+        GUI.skin = menu_skin;
+        GUI.Box(new Rect(0, 0, NATIVE_WIDTH, NATIVE_HEIGHT), "Controls");
 
-        GUI.Label(new Rect(((Screen.width / 3) + 10), 140, ((Screen.width / 3) - 20), 20), "<Controls Go Here>");
+        GUI.Label(new Rect((NATIVE_WIDTH / 2) - 250, (NATIVE_HEIGHT / 2) - 200, 480, 20), "Keys 1 through 4 switch the player between 1: Roll 2: Walk 3: Aim 4: Grasp");
+        GUI.Label(new Rect((NATIVE_WIDTH / 2) - 250, (NATIVE_HEIGHT / 2) - 180, 480, 20), "");
+        GUI.Label(new Rect((NATIVE_WIDTH / 2) - 250, (NATIVE_HEIGHT / 2) - 160, 480, 20), "Roll Mode: W: Roll forward S: Roll backward A: Roll left D: Roll right Space: Speed boost'");
+        GUI.Label(new Rect((NATIVE_WIDTH / 2) - 250, (NATIVE_HEIGHT / 2) - 140, 480, 20), "Mouse Look: Orbit camera around the character. Movement follows camera direction");
+        GUI.Label(new Rect((NATIVE_WIDTH / 2) - 250, (NATIVE_HEIGHT / 2) - 120, 480, 20), "Mouse Scroll: Dolly camera toward or away from character");
+        GUI.Label(new Rect((NATIVE_WIDTH / 2) - 250, (NATIVE_HEIGHT / 2) - 100, 480, 20), "");
+        GUI.Label(new Rect((NATIVE_WIDTH / 2) - 250, (NATIVE_HEIGHT / 2) - 80, 480, 20), "Walk Mode: W: Walk forward S: Walk backward A: Strafe left D: Strafe right");
+        GUI.Label(new Rect((NATIVE_WIDTH / 2) - 250, (NATIVE_HEIGHT / 2) - 60, 480, 20), "Space: Jump. Mouse look: Orbit camera around the character. ");
+        GUI.Label(new Rect((NATIVE_WIDTH / 2) - 250, (NATIVE_HEIGHT / 2) - 40, 480, 20), "Mouse scroll: Dolly camera toward or away from character");
+        GUI.Label(new Rect((NATIVE_WIDTH / 2) - 250, (NATIVE_HEIGHT / 2) - 20, 480, 20), "");
+        GUI.Label(new Rect((NATIVE_WIDTH / 2) - 250, (NATIVE_HEIGHT / 2) + 0, 480, 20), "Aim mode: W: Move forward. S: Move backward. A: Strafe left. D: Strafe right");
+        GUI.Label(new Rect((NATIVE_WIDTH / 2) - 250, (NATIVE_HEIGHT / 2) + 20, 480, 20), "Space: Jump. Mouse look: Rotate camera direction. Left Moude Button: Fire grapple.");
+        GUI.Label(new Rect((NATIVE_WIDTH / 2) - 250, (NATIVE_HEIGHT / 2) + 40, 480, 20), "");
+        GUI.Label(new Rect((NATIVE_WIDTH / 2) - 250, (NATIVE_HEIGHT / 2) + 60, 480, 20), "Grasp: W: Move forward. S: Move backward. A: Strafe left. D: Strafe right");
+        GUI.Label(new Rect((NATIVE_WIDTH / 2) - 250, (NATIVE_HEIGHT / 2) + 80, 480, 20), "Mouse look: Change camera direction");
+        GUI.Label(new Rect((NATIVE_WIDTH / 2) - 250, (NATIVE_HEIGHT / 2) + 100, 480, 20), "Left Mouse Button: Open/Close grasping mechanism. Toggle");
+        GUI.Label(new Rect((NATIVE_WIDTH / 2) - 250, (NATIVE_HEIGHT / 2) + 120, 480, 20), "Right Mouse Button: Rotate grasping mechanism clockwise");
+        GUI.Label(new Rect((NATIVE_WIDTH / 2) - 250, (NATIVE_HEIGHT / 2) + 140, 480, 20), "Shift W,S: Move arm and camera up or down vertically");
+        GUI.Label(new Rect((NATIVE_WIDTH / 2) - 250, (NATIVE_HEIGHT / 2) + 160, 480, 20), "Shift A,D: Increase or decrease the distance between two of the claw digits");
 
-    }//end GetControlsMenu()
+        if (GUI.Button(new Rect((NATIVE_WIDTH / 2) - 250, (NATIVE_HEIGHT / 2) + 190, 100, 20), "Back")) {
+            menu_level = enMenuLevel.pause_menu;
+        }
+        GUI.skin = null;
+
+    }
+
+    void ResetCameraValues() {
+        camera.clearFlags = flags_storage;
+        camera.cullingMask = culling_storage;
+    }
+
+    void SaveCameraValues() {
+        flags_storage = camera.clearFlags;
+        culling_storage = camera.cullingMask;
+        camera.clearFlags = CameraClearFlags.Nothing;
+        camera.cullingMask = 0;
+    }
 }
--- Assets/HorizonGame/scripts/MainMenuScript.cs
+++ Assets/HorizonGame/scripts/MainMenuScript.cs
@@ -1,149 +1,151 @@
+/*
+ * Main Menu Script
+ */
 using UnityEngine;
 using System.Collections;
 
 public class MainMenuScript : MonoBehaviour {
+
+    private static float NATIVE_WIDTH = 1024;
+    private static float NATIVE_HEIGHT = 768;
 
     public Font game_font;
 
     public float mainMenu = Screen.height;
     private bool loadMain = false;
     private bool isClick = true;
-	private bool showCredits = false;
-	
-	// The array of GUIText elements to display and scroll
-	public GUIText[] textElements;
-	
-	// The delay time before displaying the GUIText elements
+    private bool showCredits = false;
+
+    // The array of GUIText elements to display and scroll
+    public GUIText[] textElements;
+
+    // The delay time before displaying the GUIText elements
     public float displayTime = 5.0f;
-	// The delay time before starting the GUIText scroll
+    // The delay time before starting the GUIText scroll
     public float scrollTime = 5.0f;
-	// The scrolling speed
+    // The scrolling speed
     public float scrollSpeed = 0.2f;
-	
-	
-	void Update () 
-	{
-    
-        if(mainMenu != 200.0f){
+
+
+    void Update() {
+
+        if (mainMenu != 200.0f) {
             mainMenu += (4.0f);
         }
-	
-		if(showCredits==true){
-			displayTime -= Time.deltaTime;
 
-			if (displayTime < 0)
-			{
-				// if it is time to display, start the scrolling count down timer	
-				scrollTime -= Time.deltaTime;
-			}         
-			// if it is time to scroll, cycle through the GUIElements and
-					// increase their Y position by the desired speed
-			if (scrollTime < 0)
-			{
-				foreach (GUIText text in textElements)
-				{
-					text.transform.Translate(Vector3.up * scrollSpeed);
-				}
-			}
-		}		
-	}//end update
-	
-		
+        if (showCredits == true) {
+            displayTime -= Time.deltaTime;
+
+            if (displayTime < 0) {
+                // if it is time to display, start the scrolling count down timer	
+                scrollTime -= Time.deltaTime;
+            }
+            // if it is time to scroll, cycle through the GUIElements and
+            // increase their Y position by the desired speed
+            if (scrollTime < 0) {
+                foreach (GUIText text in textElements) {
+                    text.transform.Translate(Vector3.up * scrollSpeed);
+                }
+            }
+        }
+    }//end update
+
+
     //GUI Block
     void OnGUI() {
 
+        HUDScaleScript.BeginGUI();
+
         //GUI.skin.font = game_font;
-        if(isClick){
-			if(GUI.Button(new Rect(((Screen.width /4) + 100), mainMenu, ((Screen.width / 2) - 200), 20), "Click here to begin your journy"))
-			{ 
-				loadMain =true; 
-				isClick =false;
-			}
+        if (isClick) {
+            if (GUI.Button(new Rect(((NATIVE_WIDTH / 4) + 100), mainMenu, ((NATIVE_WIDTH / 2) - 200), 20), "Click here to begin your journy")) {
+                loadMain = true;
+                isClick = false;
+            }
         }
-		
-		if(loadMain==true)
-        {
-		
+
+        if (loadMain == true) {
+
             //make background box
             //200
-            GUI.Box(new Rect((Screen.width / 4), mainMenu, (Screen.width / 2), 240), "Main Menu");
+            GUI.Box(new Rect((NATIVE_WIDTH / 4), mainMenu, (NATIVE_WIDTH / 2), 240), "Main Menu");
 
             //make "Continue Journey" button
             //240
-            if (GUI.Button(new Rect(((Screen.width / 4) + 100), mainMenu + 40, ((Screen.width / 2) - 200), 20),     "Continue Journey")) {
+            if (GUI.Button(new Rect(((NATIVE_WIDTH / 4) + 100), mainMenu + 40, ((NATIVE_WIDTH / 2) - 200), 20), "Continue Journey")) {
                 Application.LoadLevel(1);
             }
 
             //make "New Journey" button
             //280
-            if (GUI.Button(new Rect(((Screen.width / 4) + 100), mainMenu+80, ((Screen.width / 2) - 200), 20), "New Journey")) {
+            if (GUI.Button(new Rect(((NATIVE_WIDTH / 4) + 100), mainMenu + 80, ((NATIVE_WIDTH / 2) - 200), 20), "New Journey")) {
                 print("This will start a New Journey");
             }
 
             //make "Options" button
             //320
-            if (GUI.Button(new Rect(((Screen.width / 4) + 100), mainMenu+120, ((Screen.width / 2) - 200), 20), "Options")) {
+            if (GUI.Button(new Rect(((NATIVE_WIDTH / 4) + 100), mainMenu + 120, ((NATIVE_WIDTH / 2) - 200), 20), "Options")) {
                 print("This will show the option screen");
             }
 
             //make "Credits" button
             //360
-            if (GUI.Button(new Rect(((Screen.width / 4) + 100), mainMenu+160, ((Screen.width / 2) - 200), 20), "Credits")) {
-                showCredits=true;
-				loadMain=false;
+            if (GUI.Button(new Rect(((NATIVE_WIDTH / 4) + 100), mainMenu + 160, ((NATIVE_WIDTH / 2) - 200), 20), "Credits")) {
+                showCredits = true;
+                loadMain = false;
             }
-    
+
             //make "Exit" button
             //400
-            if (GUI.Button(new Rect(((Screen.width / 4) + 100), mainMenu+200, ((Screen.width / 2) - 200), 20), "Exit")){
+            if (GUI.Button(new Rect(((NATIVE_WIDTH / 4) + 100), mainMenu + 200, ((NATIVE_WIDTH / 2) - 200), 20), "Exit")) {
                 Application.Quit();
             }
         }//end load main if
-		
-		if(showCredits==true){
-			displayCredits();
-		}
-		
+
+        if (showCredits == true) {
+            displayCredits();
+        }
+
+        HUDScaleScript.EndGUI();
+
     }//end GUI block
 
-	void displayCredits(){
-		
-		GUI.Box(new Rect((Screen.width / 4), 160, (Screen.width / 2), 400), "Credits");
-		
-		if(GUI.Button (new Rect(Screen.width - 350, 160, 50, 50), "Back")){
-			loadMain = true;
-			showCredits = false;
-		}
-		
-		GUI.Label(new Rect(480, 175, ((Screen.width / 3) - 20), 20), "NMC 499: Video Game Creation.");
-		GUI.Label(new Rect(460, 190, ((Screen.width / 3) - 20), 20), "New Media Communications program");
-		GUI.Label(new Rect(500, 205, ((Screen.width / 3) - 20), 20), "Oregon State University");
-		
-		
-		GUI.Label(new Rect(300, 220, ((Screen.width / 3) - 20), 20), "Instructer/Producer: Todd Kesterson");
-		GUI.Label(new Rect(300, 235, ((Screen.width / 3) - 20), 20), "Technical Director: Islam Almusaly");
-		GUI.Label(new Rect(300, 250, ((Screen.width / 3) - 20), 20), "Creative Director and Narrative Director: Dan Felder");
-		GUI.Label(new Rect(300, 265, ((Screen.width / 3) - 20), 20), "Lead Game Programmer: Garret Fleenor");
-		GUI.Label(new Rect(300, 280, ((Screen.width / 3) - 20), 20), "Concept Sorcerer and Art Director: Fari 'Bandit' Nguyen");
-		GUI.Label(new Rect(300, 295, ((Screen.width / 3) - 20), 20), "Associate Producer: Jacob Hart");
-		GUI.Label(new Rect(300, 310, ((Screen.width / 3) - 20), 20), "Technical Artist: Justin Field");
-		GUI.Label(new Rect(300, 325, ((Screen.width / 3) - 20), 20), "Modeling and Animation: Nick Harper" );
-		GUI.Label(new Rect(300, 340, ((Screen.width / 3) - 20), 20), "Modeling and Interface Design: Keenan Carr" );
-		GUI.Label(new Rect(300, 355, ((Screen.width / 3) - 20), 20), "Interface Design and Programming: Jonathan Gill" );
-		GUI.Label(new Rect(300, 370, ((Screen.width / 3) - 20), 20), "Interface Programming: Kyle Connor" );
-		GUI.Label(new Rect(300, 385, ((Screen.width / 3) - 20), 20), "Sound Design and Modeling: Maruc 'PuffballsUnited' Bromander" );
-		GUI.Label(new Rect(300, 400, ((Screen.width / 3) - 20), 20), "Level Designer (Environment): Courtland B Dastyck" );
-		GUI.Label(new Rect(300, 415, ((Screen.width / 3) - 20), 20), "Graphic Magician (Programmer): Padraic Mc Graw" );
-		GUI.Label(new Rect(300, 430, ((Screen.width / 3) - 20), 20), "Game Programming: Adam Keaton" );
-		GUI.Label(new Rect(300, 445, ((Screen.width / 3) - 20), 20), "Game Programming: Mark Udarbew" );
-		GUI.Label(new Rect(300, 460, ((Screen.width / 3) - 20), 20), "Game Programming: Micheal Tichenor" );
-		GUI.Label(new Rect(300, 475, ((Screen.width / 3) - 20), 20), "Concept Art and Modeling:Amy Liu" );
-		GUI.Label(new Rect(300, 490, ((Screen.width / 3) - 20), 20), "Modeling and Animation: Jason Monahan" );
-		GUI.Label(new Rect(300, 505, ((Screen.width / 3) - 20), 20), "Design Assistant: Mark Ritzman" );
-		GUI.Label(new Rect(300, 520, ((Screen.width / 3) - 20), 20), "Luke Miller: TBD" );
-		
-	}
-	
+    void displayCredits() {
+
+        GUI.Box(new Rect((NATIVE_WIDTH / 4), 160, (NATIVE_WIDTH / 2), 400), "Credits");
+
+        if (GUI.Button(new Rect(NATIVE_WIDTH - 350, 160, 50, 50), "Back")) {
+            loadMain = true;
+            showCredits = false;
+        }
+
+        GUI.Label(new Rect(480, 175, ((NATIVE_WIDTH / 3) - 20), 20), "NMC 499: Video Game Creation.");
+        GUI.Label(new Rect(460, 190, ((NATIVE_WIDTH / 3) - 20), 20), "New Media Communications program");
+        GUI.Label(new Rect(500, 205, ((NATIVE_WIDTH / 3) - 20), 20), "Oregon State University");
+
+
+        GUI.Label(new Rect(300, 220, ((NATIVE_WIDTH / 3) - 20), 20), "Instructer/Producer: Todd Kesterson");
+        GUI.Label(new Rect(300, 235, ((NATIVE_WIDTH / 3) - 20), 20), "Technical Director: Islam Almusaly");
+        GUI.Label(new Rect(300, 250, ((NATIVE_WIDTH / 3) - 20), 20), "Creative Director and Narrative Director: Dan Felder");
+        GUI.Label(new Rect(300, 265, ((NATIVE_WIDTH / 3) - 20), 20), "Lead Game Programmer: Garret Fleenor");
+        GUI.Label(new Rect(300, 280, ((NATIVE_WIDTH / 3) - 20), 20), "Concept Sorcerer and Art Director: Fari 'Bandit' Nguyen");
+        GUI.Label(new Rect(300, 295, ((NATIVE_WIDTH / 3) - 20), 20), "Associate Producer: Jacob Hart");
+        GUI.Label(new Rect(300, 310, ((NATIVE_WIDTH / 3) - 20), 20), "Technical Artist: Justin Field");
+        GUI.Label(new Rect(300, 325, ((NATIVE_WIDTH / 3) - 20), 20), "Modeling and Animation: Nick Harper");
+        GUI.Label(new Rect(300, 340, ((NATIVE_WIDTH / 3) - 20), 20), "Modeling and Interface Design: Keenan Carr");
+        GUI.Label(new Rect(300, 355, ((NATIVE_WIDTH / 3) - 20), 20), "Interface Design and Programming: Jonathan Gill");
+        GUI.Label(new Rect(300, 370, ((NATIVE_WIDTH / 3) - 20), 20), "Interface Programming: Kyle Connor");
+        GUI.Label(new Rect(300, 385, ((NATIVE_WIDTH / 3) - 20), 20), "Sound Design and Modeling: Maruc 'PuffballsUnited' Bromander");
+        GUI.Label(new Rect(300, 400, ((NATIVE_WIDTH / 3) - 20), 20), "Level Designer (Environment): Courtland B Dastyck");
+        GUI.Label(new Rect(300, 415, ((NATIVE_WIDTH / 3) - 20), 20), "Graphic Magician (Programmer): Padraic Mc Graw");
+        GUI.Label(new Rect(300, 430, ((NATIVE_WIDTH / 3) - 20), 20), "Game Programming: Adam Keaton");
+        GUI.Label(new Rect(300, 445, ((NATIVE_WIDTH / 3) - 20), 20), "Game Programming: Mark Udarbew");
+        GUI.Label(new Rect(300, 460, ((NATIVE_WIDTH / 3) - 20), 20), "Game Programming: Micheal Tichenor");
+        GUI.Label(new Rect(300, 475, ((NATIVE_WIDTH / 3) - 20), 20), "Concept Art and Modeling:Amy Liu");
+        GUI.Label(new Rect(300, 490, ((NATIVE_WIDTH / 3) - 20), 20), "Modeling and Animation: Jason Monahan");
+        GUI.Label(new Rect(300, 505, ((NATIVE_WIDTH / 3) - 20), 20), "Design Assistant: Mark Ritzman");
+        GUI.Label(new Rect(300, 520, ((NATIVE_WIDTH / 3) - 20), 20), "Luke Miller: TBD");
+
+    }
+
 }
-
-
